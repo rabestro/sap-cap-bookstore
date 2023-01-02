24cdd77b1b3178554151d5f89a2a1d570cc17428
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-02 22:03:14 +0200

    saving the review to the database

--- srv/src/main/java/rabestro/bookstore/handlers/CatalogServiceHandler.java
+++ srv/src/main/java/rabestro/bookstore/handlers/CatalogServiceHandler.java
@@ -3,7 +3,9 @@ package rabestro.bookstore.handlers;
 import cds.gen.catalogservice.AddReviewContext;
 import cds.gen.catalogservice.CatalogService_;
 import cds.gen.catalogservice.Reviews;
+import cds.gen.catalogservice.Reviews_;
 import cds.gen.rabestro.bookshop.Books;
+import com.sap.cds.ql.Insert;
 import com.sap.cds.ql.cqn.CqnAnalyzer;
 import com.sap.cds.reflect.CdsModel;
 import com.sap.cds.services.handler.EventHandler;
@@ -25,11 +27,18 @@ public class CatalogServiceHandler implements EventHandler {
 
     @On(event = AddReviewContext.CDS_NAME)
     public void addReview(AddReviewContext context) {
+        db.run(Insert
+                .into(Reviews_.CDS_NAME)
+                .entry(review(context)));
+    }
+
+    private Reviews review(AddReviewContext context) {
         var review = Reviews.create();
         review.setBookId(bookId(context));
         review.setTitle(context.getTitle());
         review.setText(context.getText());
         review.setRating(context.getRating());
+        return review;
     }
 
     private String bookId(AddReviewContext context) {
