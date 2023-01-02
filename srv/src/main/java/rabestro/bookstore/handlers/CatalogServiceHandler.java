13c6f8fe194f3f347f198738adfc98860060186b
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-02 21:57:31 +0200

    extracting the id from the cqn using the cqnanalyzer

--- srv/src/main/java/rabestro/bookstore/handlers/CatalogServiceHandler.java
+++ srv/src/main/java/rabestro/bookstore/handlers/CatalogServiceHandler.java
@@ -3,20 +3,39 @@ package rabestro.bookstore.handlers;
 import cds.gen.catalogservice.AddReviewContext;
 import cds.gen.catalogservice.CatalogService_;
 import cds.gen.catalogservice.Reviews;
+import cds.gen.rabestro.bookshop.Books;
+import com.sap.cds.ql.cqn.CqnAnalyzer;
+import com.sap.cds.reflect.CdsModel;
 import com.sap.cds.services.handler.EventHandler;
 import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.persistence.PersistenceService;
 import org.springframework.stereotype.Component;
 
 @Component
 @ServiceName(CatalogService_.CDS_NAME)
 public class CatalogServiceHandler implements EventHandler {
+    private final PersistenceService db;
+    private final CqnAnalyzer analyzer;
+
+    public CatalogServiceHandler(PersistenceService db, CdsModel model) {
+        this.analyzer = CqnAnalyzer.create(model);
+        this.db = db;
+    }
 
     @On(event = AddReviewContext.CDS_NAME)
     public void addReview(AddReviewContext context) {
         var review = Reviews.create();
+        review.setBookId(bookId(context));
         review.setTitle(context.getTitle());
         review.setText(context.getText());
         review.setRating(context.getRating());
+    }
+
+    private String bookId(AddReviewContext context) {
+        return (String) analyzer
+                .analyze(context.getCqn())
+                .targetKeys()
+                .get(Books.ID);
     }
 }
