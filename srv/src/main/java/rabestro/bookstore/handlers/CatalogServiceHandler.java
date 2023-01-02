11945178efb8663f110175ad79d1fc1351ceeb32
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-01-02 20:40:26 +0200

    the ‘on’ lifecycle method

--- srv/src/main/java/rabestro/bookstore/handlers/CatalogServiceHandler.java
+++ srv/src/main/java/rabestro/bookstore/handlers/CatalogServiceHandler.java
@@ -1,11 +1,18 @@
 package rabestro.bookstore.handlers;
 
+import cds.gen.catalogservice.AddReviewContext;
 import cds.gen.catalogservice.CatalogService_;
 import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
 import org.springframework.stereotype.Component;
 
 @Component
 @ServiceName(CatalogService_.CDS_NAME)
 public class CatalogServiceHandler implements EventHandler {
+
+    @On(event = AddReviewContext.CDS_NAME)
+    public void addReview() {
+
+    }
 }
